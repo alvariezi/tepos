04a602594e0b78725ec1a98f06561ce583e797cd
EkallFH <mhmmdfkrihaikall@gmail.com>
2024-12-25 14:18:27 +0700

    add api key

---
+++ src/middleware.js
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req) {
+  const apiKey = req.headers.get("x-api-key");
+
+  if (apiKey !== process.env.API_KEY) {
+    return NextResponse.json({ message: "Invalid API Key" }, { status: 401 });
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/api/(.*)"],
+};
